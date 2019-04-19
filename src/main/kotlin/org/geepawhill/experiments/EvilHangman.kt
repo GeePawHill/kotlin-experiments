2eb27d3b16643cf4b244596e9c32ccff03115c29
GeePawHill <geepawhill@geepawhill.org>
2019-04-18 21:46:22 -0400

    i am the only one who commits

--- src/main/kotlin/org/geepawhill/experiments/EvilHangman.kt
+++ src/main/kotlin/org/geepawhill/experiments/EvilHangman.kt
@@ -1,5 +1,16 @@
 package org.geepawhill.experiments
 
 class EvilHangman(val wordLength: Int) {
+    val dictionary = mutableListOf("a", "i")
+
+    var hungMon: String = "_"
+
+    fun guess(letter: Char) {
+        if (dictionary.size == 1 && dictionary.contains(letter.toString())) {
+            hungMon = letter.toString()
+        } else {
+            dictionary -= letter.toString()
+        }
+    }
 
 }
--- src/test/kotlin/org/geepawhill/experiments/EvilHangmanTest.kt
+++ src/test/kotlin/org/geepawhill/experiments/EvilHangmanTest.kt
@@ -10,4 +10,33 @@ class EvilHangmanTest {
 
         assertThat(hangman.wordLength).isEqualTo(10)
     }
+
+    @Test
+    fun `guess a letter and it's not found`() {
+        val hangman = EvilHangman(wordLength = 1)
+
+        hangman.guess('a')
+
+        assertThat(hangman.hungMon).isEqualTo("_")
+    }
+
+    @Test
+    fun `guess a letter and it is found`() {
+        val hangman = EvilHangman(wordLength = 1)
+
+        hangman.guess('a')
+        hangman.guess('i')
+
+        assertThat(hangman.hungMon).isEqualTo("i")
+    }
+
+    @Test
+    fun `guess one letter in the other order`() {
+        val hangman = EvilHangman(wordLength = 1)
+
+        hangman.guess('i')
+        hangman.guess('a')
+
+        assertThat(hangman.hungMon).isEqualTo("a")
+    }
 }
