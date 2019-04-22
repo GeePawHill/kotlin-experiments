3de1b19106f219a514a04104b785cfcc1e1246eb
GeePawHill <geepawhill@geepawhill.org>
2019-04-22 16:49:55 -0400

    Improved failed guess tests.

--- src/main/kotlin/org/geepawhill/hangman/Response.kt
+++ src/main/kotlin/org/geepawhill/hangman/Response.kt
@@ -27,14 +27,16 @@ class Response(
         return makeMissResponse(letter, badGuessesAllowed)
     }
 
-    private fun isHit(letter: Char): Boolean = dictionary[0].contains(letter)
+    private fun isHit(letter: Char): Boolean {
+        return dictionary.filter { !it.contains(letter) }.isEmpty()
+    }
 
     private fun missIsDuplicate(letter: Char) = badGuesses.contains(letter)
 
     private fun makeMissResponse(letter: Char, badGuessesAllowed: Int): Response {
         val newStatus = if (badGuesses.length == badGuessesAllowed - 1) Status.LOST
         else Status.ONGOING
-        return Response(dictionary[0], revealed, newStatus, badGuesses + letter)
+        return Response(dictionary.filter { !it.contains(letter) }, revealed, newStatus, badGuesses + letter)
     }
 
     private fun makeHitResponse(letter: Char): Response {
--- src/test/kotlin/org/geepawhill/hangman/ResponseTest.kt
+++ src/test/kotlin/org/geepawhill/hangman/ResponseTest.kt
@@ -9,11 +9,24 @@ class ResponseTest {
     private val badGuessesAllowed=10
 
     @Test
-    fun `failed guess`()
+    fun `failed guess means no change to revealed`()
     {
         val response = Response("COW", "___", Response.Status.ONGOING).guess('A', badGuessesAllowed)
-        assertThat(response.isWin).isFalse()
         assertThat(response.revealed).isEqualTo("___")
+    }
+
+    @Test
+    fun `failed guesses are recorded`()
+    {
+        val response = Response("COW", "___", Response.Status.ONGOING).guess('A', badGuessesAllowed)
+        assertThat(response.badGuesses).isEqualTo("A")
+    }
+
+    @Test
+    fun `failed guesses are appended`()
+    {
+        val response = Response("COW", "___", Response.Status.ONGOING, "A").guess('B', badGuessesAllowed)
+        assertThat(response.guess('B', badGuessesAllowed).badGuesses).isEqualTo("AB")
     }
 
     @Test
@@ -23,18 +36,59 @@ class ResponseTest {
     }
 
     @Test
-    fun `duplicate success letters in word`() {
+    fun `success guess`() {
+        val response = Response("ABC", "___", Response.Status.ONGOING, "").guess('A', badGuessesAllowed)
+        assertThat(response.status).isEqualTo(Response.Status.ONGOING)
+        assertThat(response.revealed).isEqualTo("A__")
+    }
+
+    @Test
+    fun `success guess is last character`()
+    {
+        val response = Response("ABC", "___", Response.Status.ONGOING).guess('C', badGuessesAllowed)
+        assertThat(response.revealed).isEqualTo("__C")
+    }
+
+    @Test
+    fun `reveals duplicate success letters`() {
         val response = Response("MAMA", "____", Response.Status.ONGOING).guess('A', badGuessesAllowed)
         assertThat(response.revealed).isEqualTo("_A_A")
     }
 
     @Test
-    fun `Failed guesses are recorded`()
+    fun `success guess wins`()
     {
-        val response = Response("COW", "___", Response.Status.ONGOING).guess('A', badGuessesAllowed)
-        assertThat(response.badGuesses).isEqualTo("A")
-        assertThat(response.guess('B', badGuessesAllowed).badGuesses).isEqualTo("AB")
+        val response = Response("ABC", "AB_", Response.Status.ONGOING).guess('C', badGuessesAllowed)
+        assertThat(response.status).isEqualTo(Response.Status.WON)
+        assertThat(response.revealed).isEqualTo("ABC")
     }
+
+
+
+
+
+
+
+    @Test
+    fun `reduce list by hit letters`()
+    {
+        val response = Response(listOf("MA","ME"), "__", Response.Status.ONGOING).guess('M', badGuessesAllowed).guess('A',10)
+        assertThat(response.badGuesses).contains("A")
+        assertThat(response.guess('E',10).status).isEqualTo(Response.Status.WON)
+    }
+
+    @Test
+    fun `eliminates letters`()
+    {
+        val response = Response(listOf("ME","MA"), "__", Response.Status.ONGOING).guess('M', badGuessesAllowed).guess('E',10)
+        assertThat(response.badGuesses).contains("E")
+    }
+
+
+
+
+
+
 
     @Test
     fun `it is a win if this guess makes the revealed string the same as the word`()
@@ -66,11 +120,6 @@ class ResponseTest {
     }
 
 
-    @Test
-    fun `accurate guess is last character`()
-    {
-        val response = Response("ABC", "AB_", Response.Status.ONGOING).guess('C', badGuessesAllowed)
-        assertThat(response.isWin).isTrue()
-    }
+
 
 }
